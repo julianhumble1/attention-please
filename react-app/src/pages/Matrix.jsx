06a0a99a59b6272cdeb21f927e5dc3dc3236498d
Julian Humble <humblejulian@gmail.com>
2024-07-24 11:49:00 +0100

    updated matrix to use business readiness and business value

--- react-app/src/pages/Matrix.jsx
+++ react-app/src/pages/Matrix.jsx
@@ -1,105 +1,134 @@
 import "./Matrix.scss"
 import { ScatterChart } from "@mui/x-charts/ScatterChart";
-import { ThemeProvider, createTheme, useTheme, } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useEffect, useState } from "react";
-import { cheerfulFiestaPalette } from "@mui/x-charts/colorPalettes";
+import { getModels } from "../services/llm.service.js";
 
 const Matrix = () => {
-    
-  const [data, setData] = useState([]);
-  const [dataLeaders, setDataLeaders] = useState([]);
-  const [dataChallengers, setDataChallengers] = useState([]);
-  const [dataVisionaries, setDataVisionaries] = useState([]);
-  const [dataPlayers, setDataPlayers] = useState([]);
-  const [isHidden, setIsHidden] = useState(true);
 
-  const [colorMode, setColorMode] = useState("dark");
-  const newTheme = createTheme({ palette: { mode: colorMode } });
+  const newTheme = createTheme({ palette: { mode: "dark"} });
 
   const [series, setSeries] = useState([]);
 
+  const [modelData, setModelData] = useState([])
+
+  useEffect(() => {
+
+    const fetchModelData = async () => {
+      const modelRes = await getModels();
+      const first50 = modelRes.data.slice(0,50)
+      setModelData(first50)
+    }
+
+    fetchModelData()
+  }, [])
+
   useEffect(() => {
     
-    let dataArray = [];
-    // let dataLead = [];
-    // let dataChall = [];
-    // let dataVis = [];
-    // let dataPlay = [];
+    // let dataArray = [];
 
-    for (let i = 0; i < 25; i++){
-      let negativeX, negativeY;
-      if (Math.random() > 0.5) {
-          negativeX = -1
-      } else {
-          negativeX = 1
-      }
-      if (Math.random() > 0.5) {
-          negativeY = -1
-      } else {
-          negativeY = 1
-      }
-      dataArray[i] = {
-          id: `data${i}`,
-          x: Math.random() * negativeX,
-          y: Math.random() * negativeY
-      }
-    }
-    
-
-    //   for (let i = 0; i< dataArray.length; i++){
-    //     if(dataArray[i].x >= 0 && dataArray[i].y >= 0){
-    //       dataLead.push(dataArray[i]);
-    //     } else if (dataArray[i].x < 0 && dataArray[i].y >= 0) {
-    //       dataChall.push(dataArray[i]);
-    //     } else if (dataArray[i].x < 0 && dataArray[i].y < 0) {
-    //       dataPlay.push(dataArray[i]);
-    //     } else {
-    //       dataVis.push(dataArray[i]);
-    //     }
+    // for (let i = 0; i < 25; i++){
+    //   let negativeX, negativeY;
+    //   if (Math.random() > 0.5) {
+    //       negativeX = -1
+    //   } else {
+    //       negativeX = 1
+    //   }
+    //   if (Math.random() > 0.5) {
+    //       negativeY = -1
+    //   } else {
+    //       negativeY = 1
+    //   }
+    //   dataArray[i] = {
+    //       id: `data${i}`,
+    //       x: Math.random() * negativeX,
+    //       y: Math.random() * negativeY
+    //   }
     // }
-    // setDataLeaders(dataLead);
-    // setDataChallengers(dataChall);
-    // setDataPlayers(dataPlay);
-    // setDataVisionaries(dataVis);
 
-    let seriesData = dataArray.map((dataPoint) => {
-    if (dataPoint.x >= 0 && dataPoint.y >= 0) {
+    // let seriesData = dataArray.map((dataPoint) => {
+    // if (dataPoint.x >= 0 && dataPoint.y >= 0) {
+    //   return ({
+    //     label: dataPoint.id,
+    //     data: [{
+    //       x: dataPoint.x,
+    //       y: dataPoint.y,
+    //       id: dataPoint.id
+    //     }],
+    //     color: "#1f77b4"
+    //   })
+    // } else if (dataPoint.x < 0 && dataPoint.y >= 0) {
+    //   return ({
+    //     label: dataPoint.id,
+    //     data: [{
+    //       x: dataPoint.x,
+    //       y: dataPoint.y,
+    //       id: dataPoint.id
+    //     }],
+    //     color: "#ff7f0e"
+    //   })
+    // } else if (dataPoint.x < 0 && dataPoint.y < 0) {
+    //   return ({
+    //     label: dataPoint.id,
+    //     data: [{
+    //       x: dataPoint.x,
+    //       y: dataPoint.y,
+    //       id: dataPoint.id
+    //     }],
+    //     color: "#d62728"
+    //   })
+    // } else {
+    //   return ({
+    //     label: dataPoint.id,
+    //     data: [{
+    //       x: dataPoint.x,
+    //       y: dataPoint.y,
+    //       id: dataPoint.id
+    //     }],
+    //     color: "#2ca02c"
+    //   })
+    // }
+      
+      
+      
+    let seriesData = modelData.map((dataPoint) => {
+    if (dataPoint.business_readiness >= 0 && dataPoint.business_value >= 0) {
       return ({
-        label: dataPoint.id,
+        label: dataPoint.name,
         data: [{
-          x: dataPoint.x,
-          y: dataPoint.y,
-          id: dataPoint.id
+          x: dataPoint.business_readiness,
+          y: dataPoint.business_value,
+          id: dataPoint._id
         }],
         color: "#1f77b4"
       })
-    } else if (dataPoint.x < 0 && dataPoint.y >= 0) {
+    } else if (dataPoint.business_readiness < 0 && dataPoint.business_value >= 0) {
       return ({
-        label: dataPoint.id,
+        label: dataPoint.name,
         data: [{
-          x: dataPoint.x,
-          y: dataPoint.y,
-          id: dataPoint.id
+          x: dataPoint.business_readiness,
+          y: dataPoint.business_value,
+          id: dataPoint._id
         }],
         color: "#ff7f0e"
       })
-    } else if (dataPoint.x < 0 && dataPoint.y < 0) {
+    } else if (dataPoint.business_readiness < 0 && dataPoint.business_value < 0) {
       return ({
-        label: dataPoint.id,
+        label: dataPoint.name,
         data: [{
-          x: dataPoint.x,
-          y: dataPoint.y,
-          id: dataPoint.id
+          x: dataPoint.business_readiness,
+          y: dataPoint.business_value,
+          id: dataPoint._id
         }],
         color: "#d62728"
       })
     } else {
       return ({
-        label: dataPoint.id,
+        label: dataPoint.name,
         data: [{
-          x: dataPoint.x,
-          y: dataPoint.y,
-          id: dataPoint.id
+          x: dataPoint.business_readiness,
+          y: dataPoint.business_value,
+          id: dataPoint._id
         }],
         color: "#2ca02c"
       })
@@ -107,9 +136,8 @@ const Matrix = () => {
   });
 
     setSeries(seriesData);
-    },[]) 
+  }, [modelData]) 
   
-
   return (
     <div className='matrix'>
       <div className="graph">
@@ -128,30 +156,8 @@ const Matrix = () => {
           <ScatterChart
             width={600}
             height={600}
-            series={series
-              // {
-              //   label: "Leaders",
-              //   data: dataLeaders.map((v) => ({ x: v.x, y: v.y, id: v.id })),
-              //   color:"#1f77b4"
-              // },
-              // {
-              //   label: "Challengers",
-              //   data: dataChallengers.map((v) => ({ x: v.x, y: v.y, id: v.id })),
-              //   color:"#ff7f0e"
-              // },
-              // {
-              //   label: "Visionaries",
-              //   data: dataVisionaries.map((v) => ({ x: v.x, y: v.y, id: v.id })),
-              //   color:"#2ca02c"
-              // },
-              // {
-              //   label: "Players",
-              //   data: dataPlayers.map((v) => ({ x: v.x, y: v.y, id: v.id })),
-              //   color:"#d62728"
-              // },
-            }
-            // grid={{vertical:true, horizontal:true}}
-            slotProps={{ legend: { hidden: isHidden } }}
+            series={series}
+            slotProps={{ legend: { hidden: true } }}
             leftAxis={null}
             bottomAxis={null}
             xAxis={[{ min: -1, max: 1, }]}
